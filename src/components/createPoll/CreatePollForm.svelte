2aee544852cecabcceec7fd80edd8a610a870036
Andres <andresrosalespinzon@gmail.com>
2021-06-26 11:57:29 +0200

    feat: btn component with styles created

---
+++ src/components/createPoll/CreatePollForm.svelte
@@ -0,0 +1,49 @@
+<script>
+  import Button from '../../ui/button/Button.svelte';
+
+  let fields = { question: '', answerA: '', answerB: '' };
+  let errors = { question: '', answerA: '', answerB: '' };
+  let valid = false;
+
+  const submitHandler = () => {
+    // validate question
+    // validate answer a
+    // validate answer b
+  };
+</script>
+
+<form on:submit|preventDefault={submitHandler}>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" bind:value={fields.question} />
+  </div>
+  <div class="form-field">
+    <label for="answer-a">Answer A:</label>
+    <input type="text" id="answer-a" bind:value={fields.answerA} />
+  </div>
+  <div class="form-field">
+    <label for="answer-b">Answer B:</label>
+    <input type="text" id="answer-b" bind:value={fields.answerB} />
+  </div>
+  <Button type="secondary" flat={true}>Add Poll</Button>
+</form>
+
+<style>
+  form {
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  .form-field {
+    margin: 1.2rem auto;
+  }
+  input {
+    width: 100%;
+    border-radius: 0.6rem;
+  }
+  label {
+    margin: 0.8rem auto;
+    text-align: left;
+  }
+</style>
